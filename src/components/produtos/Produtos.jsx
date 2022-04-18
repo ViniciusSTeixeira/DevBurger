73ed92c55bd914636567078d6c4a200c95ba4702
Vinicius Teixeira <93617202+ViniciusSTeixeira@users.noreply.github.com>
2022-04-18 20:35:33 -0300

    :sparkles: estilização de componentes

--- src/components/produtos/Produtos.jsx
+++ src/components/produtos/Produtos.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import'../produtos/Produtos.jsx'
+import "../produtos/Produtos.jsx";
 import ProdutosBox from "./ProdutosBox";
 import burger1 from "../../images/burger1.png";
 import burger2 from "../../images/burger2.png";
 import burger3 from "../../images/burger3.png";
+import burger4 from "../../images/burger4.png";
 
 function Produtos() {
   return (
@@ -13,6 +14,7 @@ function Produtos() {
         <ProdutosBox image={burger1} title="REACT BURGER" />
         <ProdutosBox image={burger2} title="ARQUITETURA MATRIX" />
         <ProdutosBox image={burger3} title="BURGER JS " />
+        <ProdutosBox image={burger4} title="CODE MASTER" />
       </div>
     </div>
   );
