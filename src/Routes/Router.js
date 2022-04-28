c86b941a2739d6f6c3bafe2e411717432e160fd1
jonatastalves <81393641+jonatastalves@users.noreply.github.com>
2022-04-28 15:18:40 -0300

    :sparkles: feat: adicao arquivos para ver conta usuario

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route} from "react-router-dom";
+import Account from "../components/Account/Account.jsx";
 import Cadastro from "../components/Cadastre_se/Cadastre_se.jsx";
 import Home from "../components/Home/Home";
 
@@ -9,6 +10,7 @@ const Rotas = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="cadastro" element={<Cadastro />} />
+            <Route patch="account" element={<Account />} />
         </Routes>
     )
  }
