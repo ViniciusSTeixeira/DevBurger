9512964e511f375a4788f903dcd428769e19e012
Jaiane Guimaraes <jaiane.guimaraes@jaiane.guimaraes>
2022-04-28 23:19:10 -0300

    :sparkles: Cliente Post

--- src/components/Cadastre_se/Cadastre_se.jsx
+++ src/components/Cadastre_se/Cadastre_se.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button.jsx";
 import Input from "../Input/Input.jsx"
+import { toast } from "react-toastify";
+import axios from 'axios'
 import "./Cadastre_se.css"
 
 function Cadastro() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [rua, setRua] = useState("");
+  const [numero, setNumero] = useState("");
+  const [complemento, setComplemento] = useState("") 
+
+  const submitForm = async (event) => {
+    event.preventDefault();
+
+    axios.post("https://rest-api-hamburgueria.herokuapp.com/cliente", {
+      nomeSobrenome: nome,
+      email: email,
+      telefone: telefone,
+      rua: rua,
+      numero: numero,
+      complemento: complemento
+      }).then((response) => {
+          console.log(response);
+        }).catch((error) => {
+          console.log(error.response);
+        });
+    }     
+      
+      
 
   const navigate = useNavigate()
 
@@ -18,27 +45,38 @@ function Cadastro() {
           <div id="cadastro">
             <h2>CADASTRE-SE</h2>
             <p>
-              venha para nossa plataforma e receba ofertas, cupons e novidades!
+              Venha para nossa plataforma e receba ofertas, cupons e novidades!
               Assim agilizamos seu pedido conosco para garantirmos seu
               Devburguer <strong>SEM BUG</strong> nenhum!
             </p>
 
-            <form>
-              <Input label="Nome completo" />
+            <form onSubmit={submitForm}>
+              <Input  value={nome} label="Nome completo" onChange={(event) => setNome(event.target.value)}/>
 
-              <Input label="Telefone" type="tel" />
+              <Input  value={email} label="Email" type="email" onChange={(event) => setEmail(event.target.value)}/>
 
-              <Input label="Logradouro" />
+              <Input  value={telefone} label="Telefone" type="tel" onChange={(event) => setTelefone(event.target.value)}/>
 
-              <Input label="Número" />
+              <Input  value={rua} label="Rua" onChange={(event) => setRua(event.target.value)}/>
 
-              <Input label="Complemento" />
+              <Input  value={numero} label="Número" onChange={(event) => setNumero(event.target.value)}/>
+
+              <Input  value={complemento} label="Complemento" onChange={(event) => setComplemento(event.target.value)}/>
+              <br>
+              
+              </br>
+
+              <Button type="submit">Criar Conta</Button>
+
             </form>
 
-            <Button onClick={funcaoBtn}>Enviar</Button>
+            <Button onClick={funcaoBtn}>Minha conta</Button>
           </div>
         </div>
       </div>
     );
-  }
+  
+}
   export default Cadastro;
+
+ 
